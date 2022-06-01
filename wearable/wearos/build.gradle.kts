32e4ab8316237acbac7027f19aac643132c99293
Jack Boswell (boswelja) <boswelja@outlook.com>
2022-06-01 20:41:05 +1200

    Create empty wearable/wearos module

---
+++ wearable/wearos/build.gradle.kts
@@ -0,0 +1,26 @@
+plugins {
+    kotlin("android")
+    id("com.android.library")
+    id("io.gitlab.arturbosch.detekt")
+}
+
+android {
+    namespace = "com.boswelja.accomplice.wearos"
+
+    compileSdk = 32
+
+    defaultConfig {
+        targetSdk = 32
+        minSdk = 23
+    }
+}
+
+dependencies {
+    implementation("com.google.android.gms:play-services-wearable:17.1.0")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-play-services:1.6.2")
+}
+
+detekt {
+    config = files("${rootDir.absolutePath}/config/detekt/detekt-base.yml")
+    basePath = rootDir.absolutePath
+}
