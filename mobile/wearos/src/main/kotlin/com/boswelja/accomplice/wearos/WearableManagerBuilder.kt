732a334db2719eed9e83759d246c009dd08a439b
Jack Boswell (boswelja) <boswelja@outlook.com>
2022-06-07 18:52:05 +1200

    Add some missing comments

--- mobile/wearos/src/main/kotlin/com/boswelja/accomplice/wearos/WearableManagerBuilder.kt
+++ mobile/wearos/src/main/kotlin/com/boswelja/accomplice/wearos/WearableManagerBuilder.kt
@@ -4,6 +4,9 @@ import android.content.Context
 import com.boswelja.accomplice.WearableManagerBuilder
 import com.boswelja.accomplice.WearableManagerDsl
 
+/**
+ * Adds support for the WearOS platform to this WearableManager
+ */
 @WearableManagerDsl
 fun WearableManagerBuilder.registerWearOSPlatform(context: Context, applicationCapability: String) {
     addPlatform(WearOSPlatformImpl(context, applicationCapability))
--- wearable/wearos/src/main/kotlin/com/boswelja/accomplice/wearos/WearableManager.kt
+++ wearable/wearos/src/main/kotlin/com/boswelja/accomplice/wearos/WearableManager.kt
@@ -13,6 +13,13 @@ import kotlinx.coroutines.tasks.await
 import java.io.InputStream
 import java.io.OutputStream
 
+/**
+ * Manages connections with the mobile device that's connected to the wearable.
+ *
+ * @param context [Context].
+ * @param applicationCapability A unique capability used to identify the mobile device in the node
+ * map.
+ */
 class WearableManager(
     private val context: Context,
     private val applicationCapability: String
@@ -22,6 +29,16 @@ class WearableManager(
     private val channelClient by lazy { Wearable.getChannelClient(context) }
     private val capabilityClient by lazy { Wearable.getCapabilityClient(context) }
 
+    /**
+     * Sends a message to the connected mobile device. Messages have no form of synchronization, and
+     * should not be used if reliability is a concern.
+     *
+     * @param message A unique identifier for this message.
+     * @param payload An optional payload for the message.
+     *
+     * @return true if the message was sent successfully, false otherwise. Note a successful send
+     * does *not* guarantee delivery.
+     */
     suspend fun sendMessage(
         message: String,
         payload: ByteArray?
@@ -39,10 +56,14 @@ class WearableManager(
         }
     }
 
+    /**
+     * Flows all [ReceivedMessage]s from the connected mobile device.
+     */
     fun receivedMessages(): Flow<ReceivedMessage> {
         return callbackFlow {
             val target = connectedDevice()
             val callback = MessageClient.OnMessageReceivedListener { messageEvent ->
+                // Filter out unrelated messages, just in case
                 if (messageEvent.sourceNodeId == target.nodeId) {
                     trySend(
                         ReceivedMessage(
@@ -60,6 +81,15 @@ class WearableManager(
         }
     }
 
+    /**
+     * Opens an [OutputStream] to send data to the connected mobile device.
+     *
+     * @param path A unique path for the data to be sent on.
+     * @param block A block of code scoped to the [OutputStream]. The stream is closed automatically
+     * once execution has completed. Note exceptions in this block will be caught automatically.
+     *
+     * @return true if the data was sent successfully, false otherwise.
+     */
     suspend fun sendData(
         path: String,
         block: suspend OutputStream.() -> Unit
@@ -78,6 +108,15 @@ class WearableManager(
         }
     }
 
+    /**
+     * Opens an [InputStream] to receive data from the connected mobile device.
+     *
+     * @param path A unique path to listen for incoming data on.
+     * @param block A block of code scoped to the [InputStream]. The stream is closed automatically
+     * once execution has completed. Note exceptions in this block will be caught automatically.
+     *
+     * @return true if the data was received successfully, false otherwise.
+     */
     suspend fun receiveData(
         path: String,
         block: suspend InputStream.() -> Unit
@@ -96,6 +135,9 @@ class WearableManager(
         }
     }
 
+    /**
+     * Retrieves a [MobileNode] representing the mobile device paired to this wearable.
+     */
     suspend fun connectedDevice(): MobileNode {
         val nodes = capabilityClient
             .getCapability(applicationCapability, CapabilityClient.FILTER_ALL)
